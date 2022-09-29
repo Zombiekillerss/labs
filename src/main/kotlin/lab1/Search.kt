58e84a8e040281b3382dcd9681742ead1f5d0b63
КононовАА0302 <81182918+Zombiekillerss@users.noreply.github.com>
2022-09-29 09:49:32 +0300

    add class Search add fun getResult add fun openLinkId

---
+++ src/main/kotlin/lab1/Search.kt
@@ -0,0 +1,41 @@
+package lab1
+
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import java.awt.Desktop
+import java.net.URI
+import java.net.URL
+import java.net.URLEncoder
+
+class Search(var request: String) {
+    private val requestLink = "https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch="
+    private val resultLink = "https://ru.wikipedia.org/w/index.php?curid="
+
+    private var allId: List<Page> = emptyList()
+
+    init {
+        request = URLEncoder.encode(request, "UTF-8")
+        val jsonString = URL(requestLink + request).readText()
+
+        allId = getResults(jsonString)
+    }
+    private fun getResults(jsonString: String): List<Page> {
+        val newResults: MutableList<Page> = mutableListOf()
+
+        val jsonArray = Gson()
+            .fromJson(jsonString, JsonObject::class.java)
+            .getAsJsonObject("query")
+            .getAsJsonArray("search")
+
+        jsonArray.forEach {
+            newResults.add(Page(it.asJsonObject.getAsJsonPrimitive("title").toString(),
+                it.asJsonObject.getAsJsonPrimitive("pageId").toString()))
+        }
+
+        return newResults
+    }
+
+    fun openLinkIndex(index: String){
+        Desktop.getDesktop().browse(URI(resultLink + allId[index.toInt()].pageId))
+    }
+}
