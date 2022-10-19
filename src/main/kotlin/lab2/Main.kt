c1ce0353654bfc87ed3deeb1774bcf6dee78b8f3
КононовАА0302 <81182918+Zombiekillerss@users.noreply.github.com>
2022-10-19 22:46:23 +0300

    fix bugs

--- src/main/kotlin/lab2/Main.kt
+++ src/main/kotlin/lab2/Main.kt
@@ -3,12 +3,24 @@ package lab2
 fun main() {
     println("Вводите\n1.Путь до файла с названием и расширением на конце\n2.e-выход")
     do {
-        println("Путь до файла: ")
-        val path = "e"//readln()
+        print("Путь до файла: ")
+        var path = readln()
+        if (path[path.length - 1] == 'e')
+            path = "e"
         if (path != "e") {
-            val datas = OpenAllFiles(path)
-            WorkWithFiles(datas)
+            try {
+                val start = System.currentTimeMillis()
+                val data = OpenAllFiles(path)
+                val work = WorkWithData(data.listData)
+                work.printDublicateEntry()
+                work.printCountHouse()
+                val end = System.currentTimeMillis()
+                println("Время обработки файла : " + (end - start) + " милисекунд")
+            } catch (e: Exception) {
+                println(e.message)
+            }
+
+
         }
     } while (path != "e")
-    println(OpenCsvFiles("C:\\Users\\sasha\\OneDrive\\Рабочий стол\\всякая всячина\\ООП\\address.csv").test)
 }
--- src/main/kotlin/lab2/WorkWithData.kt
+++ src/main/kotlin/lab2/WorkWithData.kt
@@ -3,35 +3,32 @@ package lab2
 class WorkWithData(private val data: HashMap<Address, Int>) {
     fun printDublicateEntry() {
         println("Адресс и количество повторений: ")
-        for (address in data) {
-            println("${address.key} Повторений ${address.value}")
+        data.forEach {
+            println("${it.key} Повторений ${it.value}")
         }
     }
 
     fun printCountHouse() {
-        val listHouse = mutableListOf<Int>()
-        var countOne = 0
-        var countTwo = 0
-        var countThree = 0
-        var countFour = 0
-        var countFive = 0
+        val countHouseCity = hashMapOf<String, CountFloor>()
         for (address in data) {
-            if (!listHouse.contains(address.key.house)) {
-                when (address.key.floor) {
-                    1 -> countOne++
-                    2 -> countTwo++
-                    3 -> countThree++
-                    4 -> countFour++
-                    5 -> countFive++
-                }
-                listHouse.add(address.key.house)
+            if (!countHouseCity.containsKey(address.key.city))
+                countHouseCity[address.key.city] = CountFloor()
+            when (address.key.floor) {
+                1 -> countHouseCity[address.key.city]?.one = countHouseCity[address.key.city]?.one!! + 1
+                2 -> countHouseCity[address.key.city]?.two = countHouseCity[address.key.city]?.two!! + 1
+                3 -> countHouseCity[address.key.city]?.three = countHouseCity[address.key.city]?.three!! + 1
+                4 -> countHouseCity[address.key.city]?.four = countHouseCity[address.key.city]?.four!! + 1
+                5 -> countHouseCity[address.key.city]?.five = countHouseCity[address.key.city]?.five!! + 1
             }
         }
-        println("Выводим количество 1,2,3,4,5 этажных домов")
-        println("1 этажных домов: $countOne")
-        println("2 этажных домов: $countTwo")
-        println("3 этажных домов: $countThree")
-        println("4 этажных домов: $countFour")
-        println("5 этажных домов: $countFive")
+        countHouseCity.forEach {
+            println("Выводим количество 1,2,3,4,5 этажных домов в городе ${it.key}")
+            println("1 этажных домов: ${it.value.one}")
+            println("2 этажных домов: ${it.value.two}")
+            println("3 этажных домов: ${it.value.three}")
+            println("4 этажных домов: ${it.value.four}")
+            println("5 этажных домов: ${it.value.five}")
+        }
+
     }
 }
