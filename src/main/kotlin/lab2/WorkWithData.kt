d491b97d6d36e71e8c1441a9c4a8917b00317136
КононовАА0302 <81182918+Zombiekillerss@users.noreply.github.com>
2022-10-19 18:42:16 +0300

    add printDublicateEntry

--- src/main/kotlin/lab2/WorkWithData.kt
+++ src/main/kotlin/lab2/WorkWithData.kt
@@ -1,4 +1,15 @@
 package lab2
 
-class WorkWithData(datas: OpenAllFiles) {
+class WorkWithData(private val data: HashMap<Address, Int>) {
+    fun printDublicateEntry() {
+        for (address in data) {
+            println(address)
+        }
+    }
+
+    fun printCountHouse() {
+        for (address in data) {
+
+        }
+    }
 }
