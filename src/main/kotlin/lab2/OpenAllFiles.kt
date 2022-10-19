567b6298a87c4b710363e0e1e908150f48c0382d
КононовАА0302 <81182918+Zombiekillerss@users.noreply.github.com>
2022-10-19 16:41:53 +0300

    add method readCsv

--- src/main/kotlin/lab2/OpenAllFiles.kt
+++ src/main/kotlin/lab2/OpenAllFiles.kt
@@ -1,16 +1,41 @@
 package lab2
 
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import java.nio.file.Files
+import java.nio.file.Paths
+
 class OpenAllFiles(path: String) {
-    private lateinit var data: OpenFile
+    lateinit var listData: HashMap<Address, Int>
 
     init {
         if (path.substring(path.length - 4) == ".csv" ||
             path.substring(path.length - 4) == ".xml"
         ) {
             if (path[path.length - 3] == 'c')
-                data = OpenCsvFiles(path)
+                listData = readCsv(path)
             else if (path[path.length - 3] == 'x')
-                data = OpenXmlFiles(path)
+                listData = readCsv(path)//OpenXmlFiles(path).test
         } else throw Exception("File is not csv or xml")
+    }
+
+    private fun readCsv(inputStream: String): HashMap<Address, Int> {
+        val reader = Files.newBufferedReader(Paths.get(inputStream))
+        val lkl = HashMap<Address, Int>()
+        val csvParser = CSVParser(reader, CSVFormat.newFormat(';'))
+        var newKey: Address
+        csvParser.records.drop(1)
+            .forEach {
+                newKey = Address(it[0], it[1], it[2].toInt(), it[3].toInt())
+                if (lkl.isEmpty() || !lkl.containsKey(newKey))
+                    lkl[newKey] = 1
+                else
+                    lkl[newKey]?.let { it1 ->
+                        lkl.replace(
+                            newKey, it1, it1 + 1
+                        )
+                    }
+            }
+        return lkl
     }
 }
