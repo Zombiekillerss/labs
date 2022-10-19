583075df13dc80f68bd6b45fe6a3be8b6bd647f2
КононовАА0302 <81182918+Zombiekillerss@users.noreply.github.com>
2022-10-19 19:38:38 +0300

    add toString

--- src/main/kotlin/lab2/Address.kt
+++ src/main/kotlin/lab2/Address.kt
@@ -5,4 +5,8 @@ data class Address(
     val street: String,
     val house: Int,
     val floor: Int
-)
+) {
+    override fun toString(): String {
+        return "Адрес: '$city', '$street', $house, $floor."
+    }
+}
